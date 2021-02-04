4570832674f669e1f6922e7388b803b4e4231b90
André Paulo <andrezzera@outlook.com>
2021-02-04 20:59:05 -0300

    feat: add global style

--- src/assets/styles/globalStyle.tsx
+++ src/assets/styles/globalStyle.tsx
@@ -6,11 +6,8 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Nunito', sans-serif;
-  }
-
-  body {
-    background: rgb(235,240,245);
-    background: radial-gradient(circle, rgba(235,240,245,1) 0%, rgba(207,214,218,1) 100%);
+    outline: none;
+    border: none;
   }
 `
 
