e84178c4c8b92d4b855143c5ab3c0ca728d4355e
André Paulo <andrezzera@outlook.com>
2021-02-04 20:59:21 -0300

    feat: render home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './assets/styles/globalStyle' 
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
       <GlobalStyle/>
+      <Home/>
     </>
   );
 }
